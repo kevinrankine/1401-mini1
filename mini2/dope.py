e8b340dd2c2cfc2078279ee0d99e8df1aba24e24
Kevin Rankine <rankine@college.harvard.edu>
2016-02-21 17:35:35 -0500

    good

--- mini2/dope.py
+++ mini2/dope.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab
 
 forward = np.zeros((60, 60))
 
 def main():
-    experiment(trials = 100, light_on = [10, 20], alpha = 0.20, gamma = 0.99)
+    experiment(trials = 2000, light_on = [10, 20], alpha = 0.20, gamma = 0.99)
 
 def experiment(light_on = [10],
                trials = 100,
@@ -28,11 +29,11 @@ def experiment(light_on = [10],
 
     for i in range(trials):
         V_hat, delta = trial(X, weights, reward, alpha, gamma)
-        if i == i:
+        if i % (trials / 2) == 0:
             plot_data(V_hat, delta)
+            
 
     turn_on(X, 20, off=True)
-
     V_hat, delta = trial(X, weights, reward, alpha, gamma)
     plot_data(V_hat, delta)
 
@@ -55,19 +56,19 @@ def predict_values(weights, X):
     return np.dot(X, weights)
 
 def plot_data(V_hat, delta):
+    delta = (delta - np.mean(delta)) / np.sqrt(np.var(delta))
     fig = plt.figure()
     
     ax = fig.add_subplot(211)
     plt.ylabel('V_hat')
     ax.plot(np.array([i for i in range(0, len(V_hat))]), V_hat, c='g')
+    pylab.ylim([-1, 1])
 
     ax = fig.add_subplot(212)
     plt.ylabel('Dopamine')
     ax.plot(np.array([i for i in range(0, len(delta))]), delta, c='r')
-    
-    timer = fig.canvas.new_timer(interval = 10000)
-    timer.add_callback(lambda : plt.close())
-    timer.start()
+    pylab.ylim([-10, 10])
+
     plt.show()
     
 if __name__ == '__main__':
