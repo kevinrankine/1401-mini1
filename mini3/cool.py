f33f9bec09cd82abb446678d8651f7ec59b00ea9
Kevin Rankine <rankine@college.harvard.edu>
2016-03-09 10:58:25 -0500

    in the cut

--- mini3/cool.py
+++ mini3/cool.py
@@ -1,5 +1,6 @@
 import numpy as np
-from copy import copy 
+from copy import copy
+import matplotlib.pyplot as plt
 
 '''
 input_neurons : [(neuron, weight), ..., (neuron, weight)]
@@ -19,7 +20,7 @@ class Neuron(object):
     
     def activate(self, t):
         if len(self.input_neurons) > 0:
-            return 1.0 / (1 + np.exp(-1 * self.G * self.X(t)))
+            return 1.0 / (1 + np.exp(-1.0 * self.G * self.X(t)))
         else:
             return self.default_value
 
@@ -38,11 +39,10 @@ class Neuron(object):
         N = len(self.V)
         
         for i in range(len(self.V)):
-            noise = np.random.randn()
-            self.V[i] = max(rate * self.Vp[i] - NE + self.activate(t)  + alpha * (sum(self.Vp) - N * self.Vp[i]) + alpha * noise, 0)
-        
-        self.G = np.log(sum(self.V))
-        
+            noise = np.random.normal(0, 1)
+            noise = 0
+            self.V[i] = rate * self.Vp[i] + (1 - rate) * (self.activate(t) + alpha * (sum(self.Vp) - N * self.Vp[i])+ noise) - NE
+        self.G = sum(self.V)
         self.Vp = self.V
         self.V = [0. for _ in range(N)]
         
@@ -52,24 +52,33 @@ def main():
     input_neuron_t = Neuron(default_value = 0.0)
     input_neuron_d = Neuron(default_value = 1.0)
     
-    decision_target = Neuron(input_neurons = [(input_neuron_t, 1)], tau = 1)
-    decision_distract = Neuron(input_neurons = [(input_neuron_d, 1)], tau = 1)
+    decision_target = Neuron(input_neurons = [(input_neuron_t, 1)], tau = 0.5)
+    decision_distract = Neuron(input_neurons = [(input_neuron_d, 1)], tau = 0.5)
     
     decision_target.add_input(decision_distract, -1)
     
-    output_neuron = Neuron(input_neurons = [(decision_target, 1)])
+    output_neuron = Neuron(input_neurons = [(decision_target, 1)], tau = 0.5)
+
+    data = []
+    D = 15
+    T = 5
     
-    for t in range(10):
-        print "Activation {}".format(output_neuron.activate(t))
-        decision_target.update_vg(t, rate = 0.5, NE = 0, alpha = 1)
-    print "SOSA"
+    for t in range(D):
+        data.append(output_neuron.activate(t))
+        decision_target.update_vg(t)
+        print decision_target.G
 
     input_neuron_t.set_default(1.0)
     input_neuron_d.set_default(0.0)
 
-    for t in range(30):
-        print "Activation {}".format(output_neuron.activate(t))
-        decision_target.update_vg(t, rate = 1, NE = 0, alpha = 1)
-    
+    print "SWAP"
+
+    for t in range(D, D + T):
+        data.append(output_neuron.activate(t))
+        decision_target.update_vg(t)
+    data = np.array(data)
+
+    plt.scatter(np.linspace(0, D + T, D + T), data)
+    plt.show()
     
 main()
