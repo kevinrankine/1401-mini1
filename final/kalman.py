bb980e5333b0d1dc55f8c997665bd5867af259f9
Kevin Rankine <rankine@college.harvard.edu>
2016-04-30 17:01:36 -0400

    added updates for variance parameters, explanation

--- final/kalman.py
+++ final/kalman.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 class KalmanFilter(object):
-    def __init__(self, d_in, var_w=1, var_r=1, volatility=0.01):
+    def __init__(self, d_in, var_w=1, var_r=1, volatility=0.01, rate = 0.0):
         self.d_in = d_in
         self.var_w = var_w
         self.var_r = var_r
         self.volatility = volatility
+        self.old_var_w = var_w
+        self.old_var_r = var_r
+        self.old_volatility = volatility
+        self.rate = rate
 
         self.w = np.zeros(d_in, dtype=np.float64)
         self.cov = var_w * np.eye(d_in, dtype=np.float64)
@@ -15,9 +19,13 @@ class KalmanFilter(object):
         diffused_cov = self.cov + self.volatility * np.eye(self.d_in)
         k_gain = diffused_cov.dot(x) / (x.dot(diffused_cov.dot(x)) + self.var_r)
         cov_diff = k_gain[np.newaxis].T.dot(x.T.dot(diffused_cov)[np.newaxis])
-        
+
+        old_w = self.w
         self.w = self.w + k_gain * delta
         self.cov = diffused_cov - cov_diff
+        
+        self.var_r += self.rate / (2 * self.var_r) * ((delta ** 2 / self.var_r) - 1)
+        self.volatility += self.rate / (2 * self.volatility) * ((np.linalg.norm(self.w - old_w) ** 2 / self.volatility) - 1)
 
     def train(self, x, reward, nepochs=10):
         for i in range(nepochs):
@@ -26,6 +34,10 @@ class KalmanFilter(object):
     def zero(self):
         self.w = np.zeros(self.d_in, dtype=np.float64)
         self.cov = self.var_w * np.eye(self.d_in, dtype=np.float64)
+        
+        self.var_w = self.old_var_w
+        self.var_r = self.old_var_r
+        self.volatility = self.old_volatility
 
     def predict(self, x):
         return np.dot(self.w, x)
