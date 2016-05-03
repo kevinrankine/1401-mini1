a307bb701ddf796d7b41dcbf6278901f286a5a6c
Kevin Rankine <rankine@college.harvard.edu>
2016-05-02 23:45:31 -0400

    added KalmanFilter.plot

--- final/kalman.py
+++ final/kalman.py
@@ -1,4 +1,10 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+mpl.rcParams['figure.figsize'] = (8., 6.)
+
 
 class KalmanFilter(object):
     def __init__(self, d_in, var_w=1, var_r=1, volatility=0.01, rate = 0.0):
@@ -14,10 +20,10 @@ class KalmanFilter(object):
         self.w = np.zeros(d_in, dtype=np.float64)
         self.cov = var_w * np.eye(d_in, dtype=np.float64)
         self.deltas = []
+        self.kalmans = []
 
     def update(self, x, reward):
         delta = reward - self.predict(x)
-        self.deltas.append(delta)
         
         diffused_cov = self.cov + self.volatility * np.eye(self.d_in)
         k_gain = diffused_cov.dot(x) / (x.dot(diffused_cov.dot(x)) + self.var_r)
@@ -30,6 +36,12 @@ class KalmanFilter(object):
         self.var_r += self.rate / (2 * self.var_r) * ((delta ** 2 / self.var_r) - 1)
         self.volatility += self.rate / (2 * self.volatility) * ((np.linalg.norm(self.w - old_w) ** 2 / self.volatility) - self.d_in)
 
+        self.var_r = max(self.var_r, 0.01)
+        self.volatility = max(self.volatility, 0.01)
+
+        self.deltas.append(delta)
+        self.kalmans.append(map(lambda x : x.tolist(), k_gain))
+
     def train(self, x, reward, nepochs=10):
         for i in range(nepochs):
             self.update(x, reward)
@@ -41,7 +53,18 @@ class KalmanFilter(object):
         self.var_w = self.old_var_w
         self.var_r = self.old_var_r
         self.volatility = self.old_volatility
+        self.deltas = []
+        self.kalmans = []
 
     def predict(self, x):
         return np.dot(self.w, x)
+
+    def plot(self):
+        plt.plot(np.arange(1, len(self.deltas) + 1), self.deltas, label='$\delta_n$')
+        plt.plot(np.arange(1, len(self.kalmans) + 1), zip(*self.kalmans)[0], label='$k_A$')
+        if len(self.kalmans[0]) > 1:
+            plt.plot(np.arange(1, len(self.kalmans) + 1), zip(*self.kalmans)[1], label='$k_B$')
         
+        plt.legend()
+        plt.ylim(-3, 3)
+        plt.show()
